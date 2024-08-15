a5968aaec707b71a3023c939f202a8a8cdbe8951
Bart Naudts <Bart.Naudts@codelaser.io>
2024-08-15 12:54:13 +0200

    test for unary operator

---
+++ e2immu-java-parser/src/test/java/org/e2immu/parser/java/TestParseUnaryOperator.java
@@ -0,0 +1,89 @@
+package org.e2immu.parser.java;
+
+import org.e2immu.language.cst.api.expression.*;
+import org.e2immu.language.cst.api.info.MethodInfo;
+import org.e2immu.language.cst.api.info.TypeInfo;
+import org.e2immu.language.cst.api.statement.Block;
+import org.e2immu.language.cst.api.statement.ReturnStatement;
+import org.intellij.lang.annotations.Language;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestParseUnaryOperator extends CommonTestParse {
+
+    @Language("java")
+    private static final String INPUT = """
+            package a.b;
+            // some comment
+            class C {
+              int bitwise(int i) {
+                return ~i;
+              }
+              int bitwise2(int i) {
+                return ~~i;
+              }
+              byte bitwiseConstant() {
+                return ~10;
+              }
+              int minus(int j) {
+                return -j;
+              }
+              int minusConstant() {
+                return -10;
+              }
+            }
+            """;
+
+    @Test
+    public void test() {
+        TypeInfo typeInfo = parse(INPUT);
+
+        MethodInfo methodInfo = typeInfo.findUniqueMethod("bitwise", 1);
+        Expression e1 = rv(methodInfo);
+        if (e1 instanceof UnaryOperator un) {
+            assertEquals("~i", un.toString());
+            Expression eval = runtime.unaryOperator(un);
+            assertInstanceOf(BitwiseNegation.class, eval);
+        } else fail();
+
+        MethodInfo methodInfo2 = typeInfo.findUniqueMethod("bitwise2", 1);
+        Expression e2 = rv(methodInfo2);
+        if (e2 instanceof UnaryOperator un1 && un1.expression() instanceof UnaryOperator un) {
+            assertEquals("~~i", un1.toString());
+            Expression eval = runtime.unaryOperator(un1);
+            assertEquals("i", eval.toString());
+        } else fail();
+
+        MethodInfo methodInfo3 = typeInfo.findUniqueMethod("minus", 1);
+        Expression e3 = rv(methodInfo3);
+        if (e3 instanceof UnaryOperator un) {
+            assertEquals("-j", un.toString());
+            Expression eval = runtime.unaryOperator(un);
+            assertInstanceOf(Negation.class, eval);
+        } else fail();
+
+        MethodInfo methodInfo4 = typeInfo.findUniqueMethod("bitwiseConstant", 0);
+        Expression e4 = rv(methodInfo4);
+        if (e4 instanceof UnaryOperator un) {
+            assertEquals("~10", un.toString());
+            Expression eval = runtime.unaryOperator(un);
+            assertInstanceOf(BitwiseNegation.class, eval);
+        } else fail();
+
+        MethodInfo methodInfo5 = typeInfo.findUniqueMethod("minusConstant", 0);
+        Expression e5 = rv(methodInfo5);
+        if (e5 instanceof UnaryOperator un) {
+            assertEquals("-10", un.toString());
+            Expression eval = runtime.unaryOperator(un);
+            assertInstanceOf(IntConstant.class, eval);
+        } else fail();
+    }
+
+    private static Expression rv(MethodInfo methodInfo) {
+        if (methodInfo.methodBody().statements().get(0) instanceof ReturnStatement rs) {
+            return rs.expression();
+        }
+        throw new RuntimeException();
+    }
+}
